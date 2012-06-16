ca342d706ae9735a927a88f1ef99a1df275f1384
vasily-knk <vasily.knk@gmail.com>
2012-06-16 22:20:32 +0400

    cpp

---
+++ cpp/mem/main.cpp
@@ -0,0 +1,15 @@
+// mem.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+using std::cout;
+using std::endl;
+
+
+int main(int argc, char* argv[])
+{
+	cout << "Hello world!" << endl;
+    return 0;
+}
+
