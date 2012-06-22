75f028e806e64e65ba80903d0d6f4a79e3dde656
vasily-knk <vasily.knk@gmail.com>
2012-06-22 12:41:58 +0400

    cpp

--- cpp/mem/main.cpp
+++ cpp/mem/main.cpp
@@ -7,18 +7,8 @@ using std::cout;
 using std::endl;
 using std::vector;
 
-struct my_struct 
-{
-    int arr[100];
 
-    void *operator new(size_t size)
-    {
-
-    }
-};
-
-const size_t Size = sizeof(my_struct);
-
+template <size_t Size>
 class mem_manager
 {
 public:
@@ -27,17 +17,24 @@ public:
 public:
     struct record
     {
-        mem_manager *man;
+        //mem_manager *man;
         record *prev, *next;
+        bool is_free;
     };
 
-    record *alloc();
-    void free (record *r);
+    void *alloc();
+    void free (void *r);
+
+    void print() const;
 
 private:
     void alloc_chunk(size_t size);
 
-
+private:
+    struct chunk_record
+    {
+        size_t size;
+    };
 private:
     const size_t CHUNK_SIZE;
 
@@ -48,7 +45,8 @@ private:
 
 };
 
-mem_manager::mem_manager(size_t chunk_size)
+template <size_t Size>
+mem_manager<Size>::mem_manager(size_t chunk_size)
     : CHUNK_SIZE(chunk_size)
     , free_(NULL)
     , occupied_(NULL)
@@ -57,24 +55,29 @@ mem_manager::mem_manager(size_t chunk_size)
 }
 
 
-mem_manager::~mem_manager()
+template <size_t Size>
+mem_manager<Size>::~mem_manager()
 {
     for (vector<char*>::const_iterator it = chunks_.begin(); it != chunks_.end(); ++it)
         delete[] *it;
 }
 
 
-
-void mem_manager::alloc_chunk(size_t size)
+template <size_t Size>
+void mem_manager<Size>::alloc_chunk(size_t size)
 {
     const size_t stride = Size + sizeof(record);
-    char *pchunk = new char[stride * size];
+    char *pchunk = new char[stride * size + sizeof(chunk_record)];
     
-    char *ptr = pchunk;
+    chunk_record *pchunk_record = reinterpret_cast<chunk_record*>(pchunk);
+    pchunk_record->size = size;
+    
+    char *ptr = reinterpret_cast<char*>(pchunk_record + 1);
     for (size_t i = 0; i < size; ++i, ptr += stride)
     {
         record *r = reinterpret_cast<record*>(ptr);
-        r->man = this;
+        //r->man = this;
+        r->is_free = true;
         
         if (i == 0)
             r->prev = NULL;
@@ -84,15 +87,16 @@ void mem_manager::alloc_chunk(size_t size)
         if (i == size - 1)
             r->next = free_;
         else
-            r->next = reinterpret_cast<record*>(ptr - stride);
+            r->next = reinterpret_cast<record*>(ptr + stride);
     }
 
-    free_ = reinterpret_cast<record*>(pchunk);
+    free_ = reinterpret_cast<record*>(pchunk_record + 1);
 
     chunks_.push_back(pchunk);
 }
 
-mem_manager::record *mem_manager::alloc()
+template <size_t Size>
+void *mem_manager<Size>::alloc()
 {
     if (free_ == NULL)
         alloc_chunk (CHUNK_SIZE);
@@ -116,11 +120,17 @@ mem_manager::record *mem_manager::alloc()
     r->prev = NULL;
     occupied_ = r;
 
-    return r;
+    assert(r->is_free);
+    r->is_free = false;
+
+    return reinterpret_cast<void*>(r + 1);
 }
 
-void mem_manager::free(record* r)
+template <size_t Size>
+void mem_manager<Size>::free(void* ptr)
 {
+    record *r = reinterpret_cast<record*>(ptr) - 1;
+    
     if (r->prev != NULL)    
         r->prev->next = r->next;
     
@@ -137,12 +147,62 @@ void mem_manager::free(record* r)
     }
 
     free_ = r;
+
+    assert(!r->is_free);
+    r->is_free = true;
+}
+
+template <size_t Size>
+void mem_manager<Size>::print() const
+{
+    for (vector<char*>::const_iterator it = chunks_.begin(); it != chunks_.end(); ++it)
+    {
+        const size_t stride = Size + sizeof(record);
+        chunk_record *pchunk_record = reinterpret_cast<chunk_record*>(*it);
+        const char *ptr = reinterpret_cast<char*>(pchunk_record + 1);
+
+        cout << "Chunk " << it - chunks_.begin() << ": "; 
+        for (size_t i = 0; i < pchunk_record->size; ++i, ptr += stride)
+        {
+            const record *r = reinterpret_cast<const record*>(ptr);
+            cout << (r->is_free ? '.' : 'O');
+        }
+        cout << endl;
+    }
 }
 
 
+
+
+struct my_struct 
+{
+    int arr[100];
+
+    static void *operator new(size_t size);
+    static void operator delete(void *ptr);
+};
+
+mem_manager<sizeof my_struct> g_manager(10);
+
+void *my_struct::operator new(size_t size)
+{
+    return g_manager.alloc();
+}
+
+void my_struct::operator delete(void *ptr)
+{
+    g_manager.free(ptr);
+}
+
 int main(int argc, char* argv[])
 {
-	cout << "Hello world!" << endl;
+    my_struct *s  = new my_struct;
+
+    g_manager.print();
+
+    delete s;
+
+    g_manager.print();
     return 0;
 }
 
