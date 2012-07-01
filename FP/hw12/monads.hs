63a7bf2eb6fb7612cb3addd0cf496a206774677c
vasily-knk <vasily.knk@gmail.com>
2012-07-01 16:38:05 +0400

    FP 12

--- FP/hw12/monads.hs
+++ FP/hw12/monads.hs
@@ -1,11 +1,21 @@
 import Control.Monad.State
 import Control.Monad.Reader
 
-forState :: (MonadState s m, Num s) => s -> s -> m () -> m ()
-forState = undefined
+forState :: (MonadState s m, Num s, Ord s) => s -> s -> m () -> m ()
+forState i j m 
+    | i < j = do
+        put i
+        m
+        forState (i+1) j m
+    | otherwise = return ()
 
-forReader :: (MonadReader s m, Num s) => s -> s -> m () -> m ()
-forReader = undefined
+    
+forReader :: (MonadReader s m, Num s, Ord s) => s -> s -> m () -> m ()
+forReader  i j m 
+    | i < j = local (\_ -> i) $ do
+        m
+        forReader (i+1) j m
+    | otherwise = return ()
 
 main = do
     flip runStateT (error "foo") $ forState 0 5 $ do
--- FP/hw12/trans.hs
+++ FP/hw12/trans.hs
@@ -3,6 +3,7 @@
 import Prelude hiding (catch)
 
 import Control.Monad.Trans
+import Control.Monad
 
 newtype MaybeT m a = MaybeT { runMaybeT :: m (Maybe a) }
 
@@ -11,19 +12,24 @@ class Monad m => MonadMaybe m where
     recover :: m a -> m a -> m a
 
 instance Monad m => Monad (MaybeT m) where
-    return = undefined
-    (>>=) = undefined
-    fail = undefined
+    return = MaybeT . return . Just
+    
+    x >>= f = MaybeT $ runMaybeT x >>= helper where
+        helper Nothing = return Nothing
+        helper (Just v) = runMaybeT $ f v
+    
+    fail _ = MaybeT $ return Nothing
 
 instance Monad m => MonadMaybe (MaybeT m) where
     abort = undefined
     recover = undefined
 
+
 instance MonadTrans MaybeT where
-    lift = undefined
+    lift = MaybeT . liftM Just
 
 instance MonadIO m => MonadIO (MaybeT m) where
-    liftIO = undefined
+    liftIO = lift . liftIO
 
 ---------------------------------------------------------------
 
@@ -34,15 +40,18 @@ class Monad m => MonadEither e m | m -> e where
     catch :: m a -> (e -> m a) -> m a
 
 instance Monad m => Monad (EitherT e m) where
-    return = undefined
-    (>>=) = undefined
+    return = EitherT . return. Right
+
+    x >>= f = EitherT $ runEitherT x >>= helper where
+        helper (Left l) = return (Left l)
+        helper (Right r) = runEitherT $ f r
 
 instance Monad m => MonadEither e (EitherT e m) where
     throw = undefined
     catch = undefined
 
 instance MonadTrans (EitherT e) where
-    lift = undefined
+    lift = undefined --EitherT . liftM . Right
 
 instance MonadIO m => MonadIO (EitherT e m) where
     liftIO = undefined
