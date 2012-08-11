ddd2a8222765d8e10221a62e00d60e535b2114b5
vasily-knk <vasily.knk@gmail.com>
2012-08-12 02:43:16 +0400

    suspects

--- algo/suspects/suspects.cpp
+++ algo/suspects/suspects.cpp
@@ -6,16 +6,17 @@
 
 int main()
 {
-	size_t num_suspects, num_truth;
+	size_t num_suspects, num_truths_expected;
     
-    cin >> num_suspects >> num_truth;
+    cin >> num_suspects >> num_truths_expected;
 
     vector<vector<size_t> > positive_for_me(num_suspects);
     vector<vector<size_t> > negative_for_me(num_suspects);
-    size_t num_positive = 0;
-    size_t num_negative = 0;
+    vector<size_t> positive;
+    vector<size_t> negative;
 
-    for (size_t i = 1; i <= num_suspects; ++i)
+
+    for (size_t i = 0; i < num_suspects; ++i)
     {   
         int vote;
         cin >> vote;
@@ -26,14 +27,63 @@ int main()
         {
             const size_t index = vote - 1;
             positive_for_me[index].push_back(i);
-            ++num_positive;
+            positive.push_back(i);
         }
         else
         {
             const size_t index = -vote - 1;
             negative_for_me[index].push_back(i);
-            ++num_negative;
+            negative.push_back(i);
         }
+    }
+    
+    for (size_t i = 0; i < num_suspects; ++i)
+    {
+        std::sort(positive_for_me[i].begin(), positive_for_me[i].end());
+        std::sort(negative_for_me[i].begin(), negative_for_me[i].end());
+    }
+
+    vector<size_t> truths(num_suspects, 0);
+    //vector<size_t> lies(num_suspects, 0);
+    size_t num_cases = 0;
+
+    for (size_t i = 0; i < num_suspects; ++i)
+    {
+        const size_t num_truths = positive_for_me[i].size() + negative.size() - negative_for_me[i].size();
+        if (num_truths != num_truths_expected)
+        {
+            // definitely not murderer
+            //for (vector<size_t>::const_iterator it = positive_for_me[i].begin(); it != positive_for_me[i].end(); ++it)
+                //lies[*it] = num_suspects;
+            
+            for (vector<size_t>::const_iterator it = negative_for_me[i].begin(); it != negative_for_me[i].end(); ++it)
+                truths[*it] = num_suspects;
+
+            continue;
+        }
+
+        // can be murderer
+        for (vector<size_t>::const_iterator it = positive_for_me[i].begin(); it != positive_for_me[i].end(); ++it)
+            ++truths[*it];
+
+        for (vector<size_t>::const_iterator it = negative.begin(); it != negative.end(); ++it)
+        {
+            if (!std::binary_search(negative_for_me[i].begin(), negative_for_me[i].end(), *it))
+                ++truths[*it];
+        }
+        
+        ++num_cases;
+    }
+
+
+    for (size_t i = 0; i < num_suspects; ++i)
+    {
+        if (truths[i] >= num_cases)
+            cout << "Truth" << endl;
+        else if (truths[i] == 0)
+            cout << "Lie" << endl;
+        else
+            cout << "Not defined" << endl;
     }
 
 
